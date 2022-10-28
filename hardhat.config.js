d643a55f1084ec28a9cfec9613601177303c524c
William.Potter <rainstorm.exp@gmail.com>
2022-10-28 08:00:35 +0500

    hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,13 +3,20 @@ require("dotenv").config({ path: ".env" });
 
 const INFURA_API_URL = process.env.INFURA_API_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY;
 
 module.exports = {
   solidity: "0.8.17",
+  defaultNetwork: "mumbai",
   networks: {
-    matic: {
+    mumbai: {
       url: INFURA_API_URL,
       accounts: [PRIVATE_KEY],
     },
   },
+  etherscan: {
+    apiKey: POLYGONSCAN_API_KEY
+  }
+
+
 };
